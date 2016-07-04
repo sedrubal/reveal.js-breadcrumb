a83fe66efb0a12ea4fea9e63faee88690d3d142c
sedrubal <dev@sedrubal.de>
2016-07-04 18:49:17 +0200

    Initial commit, add js, css, example and README

---
+++ README.md
@@ -0,0 +1,25 @@
+Reveal.js Breadcrumb Plugin
+===========================
+
+Breadcrumbs plugin for reveal.js
+--------------------------------
+
+Adds breadcrumbs to the bottom of the slides generated out of the slide ID.
+
+Usage
+-----
+
+ - Add HTML IDs to your slides. If you want a breadcrumb like `Foo > Bar Section` name the title: `Foo:Bar_Section`
+ - Add `{ src: 'breadcrumb/breadcrumb.js', async: true },` to the dependencies section
+ - Add `<link rel="stylesheet" href="breadcrumb/breadcrumb.css">`
+ - If you want to display no breadcrumb on single slides, add `data-nobreadcrumb=""` to the slide. You can also add this to a encapsulating slide to hide breadcrumbs for child breadcrumbs.
+
+Example
+-------
+
+![example screenshot](./breadcrumb_example.png)
+
+License
+-------
+
+[GPLv3](https://www.gnu.org/licenses/gpl.html)
---
+++ breadcrumb.js
@@ -0,0 +1,36 @@
+/**
+ * A plugin which adds breadcrumbs to the presentation.
+ *
+ * @author sedrubal
+ */
+var RevealBreadcrumb = window.RevealBreadcrumb || (function(){
+
+  var options = Reveal.getConfig().breadcrumb || {};
+
+  // create breadcrumb bar
+  var breadcrumb_bar = document.createElement('div');
+  breadcrumb_bar.classList.add('breadcrumb');
+  document.getElementsByClassName('reveal')[0].appendChild(breadcrumb_bar);
+
+  // change breadcrumb text on slide change
+  Reveal.addEventListener( 'slidechanged', function( event ) {
+    update_breadcrumb( event.currentSlide );
+  });
+
+  // update innerHTML of all .breadcrumb
+  function update_breadcrumb( currentSlide ) {
+    var bs = document.getElementsByClassName('breadcrumb');
+    for (var i = 0; i < bs.length; i++) {
+      if (currentSlide.dataset['noBreadcrumb'] === undefined &&
+          currentSlide.parentElement.dataset['noBreadcrumb'] === undefined) {
+        bs[i].innerHTML = currentSlide.id.replace(/:/g, ' &rsaquo; ').replace(/_/g, ' ');
+        bs[i].style['opacity'] = 1;
+      } else {
+        bs[i].style['opacity'] = 0;
+      }
+    }
+  }
+
+  update_breadcrumb( Reveal.getCurrentSlide() );
+
+})();

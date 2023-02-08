aa77aa9a616c6457a93d04a37b170f99bf4d137e
sedrubal <dev@sedrubal.de>
2023-02-08 12:36:08 +0200

    Switch to npm, format and lint sources, release v1.0.2

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
-Reveal.js Breadcrumb Plugin
-===========================
+# Reveal.js Breadcrumb Plugin
 
-Breadcrumbs plugin for reveal.js
---------------------------------
+## Breadcrumbs plugin for reveal.js
 
 Adds breadcrumbs to the bottom of the slides generated out of the slide ID.
 
-Usage
------
+## Usage
 
- - Install with npm, yarn or bower: `(npm install|yarn add|bower install) reveal.js-breadcrumb`
- - Add HTML IDs to your slides. If you want a breadcrumb like `Foo > Bar Section` name the title: `Foo:Bar_Section`
- - Add `{ src: 'breadcrumb/breadcrumb.js', async: true },` to the dependencies section
- - Add `<link rel="stylesheet" href="breadcrumb/breadcrumb.css">`
- - If you want to display no breadcrumb on single slides, add `data-nobreadcrumb=""` to the slide. You can also add this to a encapsulating slide to hide breadcrumbs for child breadcrumbs.
+- Install with npm, yarn or bower: `(npm install|yarn add|bower install) reveal.js-breadcrumb`
+- Add HTML IDs to your slides. If you want a breadcrumb like `Foo > Bar Section` name the title: `Foo:Bar_Section`
+- Add `{ src: 'breadcrumb/breadcrumb.js', async: true },` to the dependencies section
+- Add `<link rel="stylesheet" href="breadcrumb/breadcrumb.css">`
+- If you want to display no breadcrumb on single slides, add `data-nobreadcrumb=""` to the slide. You can also add this to a encapsulating slide to hide breadcrumbs for child breadcrumbs.
 
-Example
--------
+## Example
 
 ![example screenshot](./breadcrumb_example.png)
 
-License
--------
+## License
 
 [GPLv3](https://www.gnu.org/licenses/gpl.html)
--- breadcrumb.js
+++ breadcrumb.js
@@ -1,36 +1,42 @@
+"use strict";
+
 /**
  * A plugin which adds breadcrumbs to the presentation.
  *
  * @author sedrubal
  */
-var RevealBreadcrumb = window.RevealBreadcrumb || (function(){
+const RevealBreadcrumb =
+  window.RevealBreadcrumb ||
+  (function () {
+      // var options = window.Reveal.getConfig().breadcrumb || {};
 
-  var options = Reveal.getConfig().breadcrumb || {};
+      // create breadcrumb bar
+      const breadcrumb_bar = document.createElement("div");
+      breadcrumb_bar.classList.add("breadcrumb");
+      document.getElementsByClassName("reveal")[0].appendChild(breadcrumb_bar);
 
-  // create breadcrumb bar
-  var breadcrumb_bar = document.createElement('div');
-  breadcrumb_bar.classList.add('breadcrumb');
-  document.getElementsByClassName('reveal')[0].appendChild(breadcrumb_bar);
+      // change breadcrumb text on slide change
+      window.Reveal.addEventListener("slidechanged", function (event) {
+          update_breadcrumb(event.currentSlide);
+      });
 
-  // change breadcrumb text on slide change
-  Reveal.addEventListener( 'slidechanged', function( event ) {
-    update_breadcrumb( event.currentSlide );
-  });
-
-  // update innerHTML of all .breadcrumb
-  function update_breadcrumb( currentSlide ) {
-    var bs = document.getElementsByClassName('breadcrumb');
-    for (var i = 0; i < bs.length; i++) {
-      if (currentSlide.dataset['nobreadcrumb'] === undefined &&
-          currentSlide.parentElement.dataset['nobreadcrumb'] === undefined) {
-        bs[i].innerHTML = currentSlide.id.replace(/:/g, ' &rsaquo; ').replace(/_/g, ' ');
-        bs[i].style['opacity'] = 1;
-      } else {
-        bs[i].style['opacity'] = 0;
+      // update innerHTML of all .breadcrumb
+      function update_breadcrumb(currentSlide) {
+          const bs = document.getElementsByClassName("breadcrumb");
+          for (var i = 0; i < bs.length; i++) {
+              if (
+                  currentSlide.dataset["nobreadcrumb"] === undefined &&
+          currentSlide.parentElement.dataset["nobreadcrumb"] === undefined
+              ) {
+                  bs[i].innerHTML = currentSlide.id
+                      .replace(/:/g, " &rsaquo; ")
+                      .replace(/_/g, " ");
+                  bs[i].style["opacity"] = 1;
+              } else {
+                  bs[i].style["opacity"] = 0;
+              }
+          }
       }
-    }
-  }
 
-  update_breadcrumb( Reveal.getCurrentSlide() );
-
-})();
+      update_breadcrumb(window.Reveal.getCurrentSlide());
+  })();
